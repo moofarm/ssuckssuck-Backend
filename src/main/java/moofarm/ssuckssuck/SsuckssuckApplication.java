fd5bb966c624652b60cff9fcdd0f7c406bb95de0
GeonHui2 <022686@naver.com>
2024-03-11 21:14:51 +0900

    :page_facing_up:: initial commit

---
+++ src/main/java/moofarm/ssuckssuck/SsuckssuckApplication.java
@@ -0,0 +1,13 @@
+package moofarm.ssuckssuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsuckssuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsuckssuckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moofarm/ssuckssuck/SsuckssuckApplicationTests.java
@@ -0,0 +1,13 @@
+package moofarm.ssuckssuck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsuckssuckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
