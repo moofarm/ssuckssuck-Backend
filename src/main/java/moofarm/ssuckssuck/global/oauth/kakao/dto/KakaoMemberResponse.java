f673e5bae0365eb79d11ebaff664edb046dbbc8a
GeonHui2 <022686@naver.com>
2024-03-23 23:04:05 +0900

    :page_facing_up:: KakaoUserInfoDto 추가(#1)

---
+++ src/main/java/moofarm/ssuckssuck/global/oauth/kakao/dto/KakaoMemberResponse.java
@@ -0,0 +1,64 @@
+package moofarm.ssuckssuck.global.oauth.kakao.dto;
+
+import java.time.LocalDateTime;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import moofarm.ssuckssuck.domain.oauth.domain.OauthMember;
+
+import static moofarm.ssuckssuck.domain.oauth.domain.OauthServerType.KAKAO;
+
+@JsonNaming(SnakeCaseStrategy.class)
+public record KakaoMemberResponse(
+        Long id,
+        boolean hasSignedUp,
+        LocalDateTime connectedAt,
+        KakaoAccount kakaoAccount
+) {
+
+    public OauthMember toDomain() {
+        return OauthMember.builder()
+                .name(kakaoAccount.profile.nickname)
+                .email(kakaoAccount.email)
+                .oauthServerType(KAKAO)
+                .build();
+    }
+
+    @JsonNaming(SnakeCaseStrategy.class)
+    public record KakaoAccount(
+            boolean profileNeedsAgreement,
+            boolean profileNicknameNeedsAgreement,
+            boolean profileImageNeedsAgreement,
+            Profile profile,
+            boolean nameNeedsAgreement,
+            String name,
+            boolean emailNeedsAgreement,
+            boolean isEmailValid,
+            boolean isEmailVerified,
+            String email,
+            boolean ageRangeNeedsAgreement,
+            String ageRange,
+            boolean birthyearNeedsAgreement,
+            String birthyear,
+            boolean birthdayNeedsAgreement,
+            String birthday,
+            String birthdayType,
+            boolean genderNeedsAgreement,
+            String gender,
+            boolean phoneNumberNeedsAgreement,
+            String phoneNumber,
+            boolean ciNeedsAgreement,
+            String ci,
+            LocalDateTime ciAuthenticatedAt
+    ) {
+    }
+
+    @JsonNaming(SnakeCaseStrategy.class)
+    public record Profile(
+            String nickname,
+            String thumbnailImageUrl,
+            String profileImageUrl,
+            boolean isDefaultImage
+    ) {
+    }
+
+}
