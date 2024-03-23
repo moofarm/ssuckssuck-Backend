252138c7d22a094dfc77d8082f5389359ffaf117
GeonHui2 <022686@naver.com>
2024-03-23 22:57:58 +0900

    :page_facing_up:: OauthServerTypeConverter 추가(#1)

---
+++ src/main/java/moofarm/ssuckssuck/domain/oauth/presentation/OauthServerTypeConverter.java
@@ -0,0 +1,13 @@
+package moofarm.ssuckssuck.domain.oauth.presentation;
+
+
+import moofarm.ssuckssuck.domain.oauth.domain.OauthServerType;
+import org.springframework.core.convert.converter.Converter;
+
+public class OauthServerTypeConverter implements Converter<String, OauthServerType> {
+
+    @Override
+    public OauthServerType convert(String source) {
+        return OauthServerType.fromName(source);
+    }
+}
