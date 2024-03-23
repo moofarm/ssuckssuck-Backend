091933794bc486bd43894326ad39a22ac22ed608
GeonHui2 <022686@naver.com>
2024-03-23 22:57:41 +0900

    :page_facing_up:: OauthServerType 추가(#1)

---
+++ src/main/java/moofarm/ssuckssuck/domain/oauth/domain/OauthServerType.java
@@ -0,0 +1,19 @@
+package moofarm.ssuckssuck.domain.oauth.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import static java.util.Locale.ENGLISH;
+
+@AllArgsConstructor
+@Getter
+public enum OauthServerType {
+    KAKAO,
+    GOOGLE,
+    NAVER,
+    APPLE;
+
+    public static OauthServerType fromName(String type) {
+        return OauthServerType.valueOf(type.toUpperCase(ENGLISH));
+    }
+}
