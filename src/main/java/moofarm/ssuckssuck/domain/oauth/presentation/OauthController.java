cc416dcecc96c195a05461a514e00c75c19ad30f
GeonHui2 <022686@naver.com>
2024-03-30 20:05:10 +0900

    :sparkles:: swagger 어노테이션 추가(#10)

--- src/main/java/moofarm/ssuckssuck/domain/oauth/presentation/OauthController.java
+++ src/main/java/moofarm/ssuckssuck/domain/oauth/presentation/OauthController.java
@@ -1,5 +1,8 @@
 package moofarm.ssuckssuck.domain.oauth.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import moofarm.ssuckssuck.domain.oauth.domain.OauthServerType;
@@ -9,6 +12,7 @@ import moofarm.ssuckssuck.domain.oauth.service.OauthService;
 import org.hibernate.annotations.Parameter;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "인증", description = "로그인 관련 API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
@@ -16,6 +20,8 @@ public class OauthController {
 
     private final OauthService oauthService;
 
+    @SecurityRequirements
+    @Operation(summary = "소셜 로그인")
     @PostMapping("/login")
     public OauthLoginResponse login(
             @RequestBody OauthLoginRequest oauthLoginRequest,
