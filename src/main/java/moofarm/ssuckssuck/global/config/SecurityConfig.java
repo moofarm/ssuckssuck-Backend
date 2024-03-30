878dae08d42415985bd811bba7aa0996049915d2
GeonHui2 <022686@naver.com>
2024-03-30 13:40:23 +0900

    :sparkles:: JwtTokenFilter 적용(#3)

--- src/main/java/moofarm/ssuckssuck/global/config/SecurityConfig.java
+++ src/main/java/moofarm/ssuckssuck/global/config/SecurityConfig.java
@@ -1,17 +1,21 @@
 package moofarm.ssuckssuck.global.config;
 
 import lombok.RequiredArgsConstructor;
+import moofarm.ssuckssuck.global.security.JwtTokenProvider;
+import moofarm.ssuckssuck.global.security.JwtTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtTokenProvider jwtTokenProvider;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.formLogin().disable().cors().and().csrf().disable();
@@ -20,7 +24,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/login").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(new JwtTokenFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
