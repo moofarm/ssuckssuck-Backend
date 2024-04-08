a4348aa06ed10dc2fc7ad4665e00d914e7ee19ff
GeonHui2 <022686@naver.com>
2024-04-08 12:44:15 +0900

    :sparkles:: UserVo 추가(#15)

---
+++ src/main/java/moofarm/ssuckssuck/domain/user/domain/vo/UserInfoVO.java
@@ -0,0 +1,16 @@
+package moofarm.ssuckssuck.domain.user.domain.vo;
+
+import moofarm.ssuckssuck.domain.oauth.domain.OauthServerType;
+import moofarm.ssuckssuck.global.common.MainCategory;
+import moofarm.ssuckssuck.global.common.SubCategory;
+
+public record UserInfoVO(
+        Long id,
+        String name,
+        String email,
+        String nickname,
+        OauthServerType oauthServerType,
+        MainCategory mainCategory,
+        SubCategory subCategory
+) {
+}
