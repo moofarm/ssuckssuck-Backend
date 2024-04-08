21882e0765f34d8b2da689cd2364e5a113bf723a
GeonHui2 <022686@naver.com>
2024-04-08 12:41:34 +0900

    :sparkles:: 유저 dto 추가(#15)

---
+++ src/main/java/moofarm/ssuckssuck/domain/user/presentation/dto/response/UserProfileResponse.java
@@ -0,0 +1,20 @@
+package moofarm.ssuckssuck.domain.user.presentation.dto.response;
+
+import moofarm.ssuckssuck.domain.oauth.domain.OauthServerType;
+import moofarm.ssuckssuck.global.common.MainCategory;
+import moofarm.ssuckssuck.global.common.SubCategory;
+import moofarm.ssuckssuck.domain.user.domain.vo.UserInfoVO;
+
+public record UserProfileResponse(
+        String email,
+        String name,
+        String nickname,
+        OauthServerType oauthServerType,
+        MainCategory mainCategory,
+        SubCategory subCategory
+) {
+    public UserProfileResponse(UserInfoVO userInfoVO) {
+        this(userInfoVO.email(), userInfoVO.name(), userInfoVO.nickname(), userInfoVO.oauthServerType(),
+                userInfoVO.mainCategory(), userInfoVO.subCategory());
+    }
+}
