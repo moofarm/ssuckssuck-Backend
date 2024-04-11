959af579d2087018329e0d4ee13dfca3bad05f5d
GeonHui2 <022686@naver.com>
2024-04-11 21:00:18 +0900

    :sparkles:: AvatarInfoVO 추가(#18)

--- src/main/java/moofarm/ssuckssuck/domain/user/presentation/dto/response/SignUpResponse.java
+++ src/main/java/moofarm/ssuckssuck/domain/user/presentation/dto/response/SignUpResponse.java
@@ -1,5 +1,6 @@
 package moofarm.ssuckssuck.domain.user.presentation.dto.response;
 
+import moofarm.ssuckssuck.domain.character.domain.vo.AvatarInfoVO;
 import moofarm.ssuckssuck.domain.oauth.domain.OauthServerType;
 import moofarm.ssuckssuck.global.common.MainCategory;
 import moofarm.ssuckssuck.global.common.SubCategory;
@@ -12,10 +13,11 @@ public record SignUpResponse (
         OauthServerType oauthServerType,
         MainCategory mainCategory,
         SubCategory subCategory,
+        AvatarInfoVO avatarInfoVO,
         Boolean isFirst
 ) {
     public SignUpResponse(UserInfoVO userInfoVO, Boolean isFirst) {
         this(userInfoVO.email(), userInfoVO.name(), userInfoVO.nickname(), userInfoVO.oauthServerType(),
-                userInfoVO.mainCategory(), userInfoVO.subCategory(), isFirst);
+                userInfoVO.mainCategory(), userInfoVO.subCategory(), userInfoVO.avatarInfoVO(), isFirst);
     }
 }
