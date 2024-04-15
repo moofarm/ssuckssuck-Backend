68ccf71cf9f7994d29161a2e1c7cb5288a2e8c7d
skdudddl <knyg99@naver.com>
2024-04-15 21:55:31 +0900

    :sparkles:: GroupController 추가(#16)

---
+++ src/main/java/moofarm/ssuckssuck/domain/group/presentation/GroupController.java
@@ -0,0 +1,39 @@
+package moofarm.ssuckssuck.domain.group.presentation;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import moofarm.ssuckssuck.domain.group.presentation.dto.request.CreateGroupRequest;
+import moofarm.ssuckssuck.domain.group.presentation.dto.response.GroupResponse;
+import moofarm.ssuckssuck.domain.group.service.GroupService;
+import moofarm.ssuckssuck.global.common.SubCategory;
+import org.springframework.data.domain.Slice;
+import org.springframework.web.bind.annotation.*;
+
+
+@Tag(name = "미션방", description = "미션방 관련 API")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/group")
+public class GroupController {
+    private final GroupService groupService;
+
+    @Operation(summary = "미션방 생성")
+    @PostMapping("/generation")
+    public GroupResponse createGroup (@RequestBody @Valid CreateGroupRequest createGroupRequest) {
+        return groupService.createGroup(createGroupRequest);
+    }
+
+    @Operation(summary = "미션방 하위 카테고리 정보 조회")
+    @GetMapping("/search/subCategory/{subCategory}/{pageNumber}")
+    public Slice<GroupResponse> getGroupsBySubCategory(@PathVariable(value = "subCategory") SubCategory subCategory, @PathVariable(value = "pageNumber") int pageNumber, @RequestParam(name = "sortBy", defaultValue = "createDate") String sortBy) {
+        return groupService.getGroupsBySubCategory(subCategory, pageNumber, sortBy);
+    }
+
+    @Operation(summary = "미션방 키워드 검색")
+    @GetMapping("/search/{pageNumber}/{keyword}")
+    public Slice<GroupResponse> getGroupsByKeyword(@PathVariable(value = "pageNumber") int pageNumber, @PathVariable(value = "keyword") String keyword) {
+        return groupService.getGroupsByKeyword(pageNumber,keyword);
+    }
+}
