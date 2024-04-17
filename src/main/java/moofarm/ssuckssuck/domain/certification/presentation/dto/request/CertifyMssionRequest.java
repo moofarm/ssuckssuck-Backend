48871afe569750a44e63a67d21e2cd16775dd91b
skdudddl <knyg99@naver.com>
2024-04-17 20:21:24 +0900

    :fire:: RequestDto 삭제(#21)

--- src/main/java/moofarm/ssuckssuck/domain/certification/presentation/dto/request/CertifyMssionRequest.java
+++
@@ -1,9 +0,0 @@
-package moofarm.ssuckssuck.domain.certification.presentation.dto.request;
-
-public record CertifyMssionRequest(
-        //Group group,
-        //User user,
-        String certificationImage,
-        int likeCount
-) {
-}
