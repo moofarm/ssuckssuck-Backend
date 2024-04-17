1478dceeca05af0447aa50cc4d091c3c764af085
skdudddl <knyg99@naver.com>
2024-04-17 20:20:47 +0900

    :sparkles:: CertificationRepository 추가(#21)

--- src/main/java/moofarm/ssuckssuck/domain/certification/domain/repository/CertificationRepository.java
+++ src/main/java/moofarm/ssuckssuck/domain/certification/domain/repository/CertificationRepository.java
@@ -1,14 +1,16 @@
 package moofarm.ssuckssuck.domain.certification.domain.repository;
 
 import moofarm.ssuckssuck.domain.certification.domain.Certification;
+import moofarm.ssuckssuck.domain.group.domain.Group;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 
 public interface CertificationRepository extends JpaRepository<Certification, Long> {
 
-    // 미션 인증 수정
-    //미션 인증 리스트 조회
-    //Slice<Certification> findAllByGroup(Group group, Pageable pageable);
-    // 미션 인증 삭제
+    Optional<Certification> findById(Long id);
+    Slice<Certification> findAllByGroup(Group group, Pageable pageable);
 }
