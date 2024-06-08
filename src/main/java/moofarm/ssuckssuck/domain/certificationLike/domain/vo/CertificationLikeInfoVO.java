d29b546cdfd0d91b10a9e47bb52c52717e282313
skdudddl <knyg99@naver.com>
2024-06-09 00:03:21 +0900

    :sparkles:: CertificationLikeInfoVO 추가(#26)

---
+++ src/main/java/moofarm/ssuckssuck/domain/certificationLike/domain/vo/CertificationLikeInfoVO.java
@@ -0,0 +1,8 @@
+package moofarm.ssuckssuck.domain.certificationLike.domain.vo;
+
+public record CertificationLikeInfoVO(
+        Long id,
+        Long certificationId,
+        boolean likeStatus
+) {
+}
