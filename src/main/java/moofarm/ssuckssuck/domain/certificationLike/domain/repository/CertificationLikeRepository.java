04aaa64f4a3d3e5982619c9ffa84cced49ffa749
skdudddl <knyg99@naver.com>
2024-06-09 00:03:02 +0900

    :sparkles:: CertificationLikeRepository 추가(#26)

--- src/main/java/moofarm/ssuckssuck/domain/certificationLike/domain/repository/CertificationLikeRepository.java
+++ src/main/java/moofarm/ssuckssuck/domain/certificationLike/domain/repository/CertificationLikeRepository.java
@@ -1,8 +1,12 @@
 package moofarm.ssuckssuck.domain.certificationLike.domain.repository;
 
+import moofarm.ssuckssuck.domain.certification.domain.Certification;
 import moofarm.ssuckssuck.domain.certificationLike.domain.CertificationLike;
-import moofarm.ssuckssuck.domain.group.domain.Group;
+import moofarm.ssuckssuck.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CertificationLikeRepository extends JpaRepository<CertificationLike, Long> {
+    Optional<CertificationLike> findByCertificationAndUser(Certification certification, User user);
 }
