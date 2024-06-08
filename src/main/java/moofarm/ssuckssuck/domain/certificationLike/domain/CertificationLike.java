d4c905907c65cb83d3f2203262ae6e3b65939262
skdudddl <knyg99@naver.com>
2024-06-08 22:09:24 +0900

    :sparkles:: CertificationLike Entity 생성(#26)

--- src/main/java/moofarm/ssuckssuck/domain/certificationLike/domain/CertificationLike.java
+++ src/main/java/moofarm/ssuckssuck/domain/certificationLike/domain/CertificationLike.java
@@ -1,9 +1,12 @@
 package moofarm.ssuckssuck.domain.certificationLike.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import moofarm.ssuckssuck.domain.certification.domain.Certification;
+import moofarm.ssuckssuck.domain.certificationLike.domain.vo.CertificationLikeInfoVO;
+import moofarm.ssuckssuck.domain.user.domain.User;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
@@ -21,5 +24,31 @@ public class CertificationLike {
     @JoinColumn(name = "certificaion_id")
     private Certification certification;
 
-    private boolean Like_status;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private boolean likeStatus;
+
+    @Builder
+    public CertificationLike(Long id, Certification certification, User user, boolean likeStatus) {
+        this.id = id;
+        this.certification = certification;
+        this.user = user;
+        this.likeStatus = likeStatus;
+    }
+    public CertificationLikeInfoVO getCertificationLikeInfoVO() {
+        return new CertificationLikeInfoVO(
+                id,
+                certification.getId(),
+                likeStatus
+        );
+    }
+    public static CertificationLike createCertificationLike(Certification certification, User user) {
+        return CertificationLike.builder()
+                .certification(certification)
+                .user(user)
+                .likeStatus(true)
+                .build();
+    }
 }
