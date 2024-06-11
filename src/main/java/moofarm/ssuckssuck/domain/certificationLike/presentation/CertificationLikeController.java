4ace16ef149905d7be20bab42d19ee0ab6ebc9be
skdudddl <knyg99@naver.com>
2024-06-11 14:14:11 +0900

    :bug:: CertificationLikeController 수정(#26)

--- src/main/java/moofarm/ssuckssuck/domain/certificationLike/presentation/CertificationLikeController.java
+++ src/main/java/moofarm/ssuckssuck/domain/certificationLike/presentation/CertificationLikeController.java
@@ -10,18 +10,15 @@ import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "미션 좋아요", description = "미션 좋아요 관련 API")
 @RestController
-@RequestMapping("/api/certificationLIke")
+@RequestMapping("/api/certificationLike")
 @RequiredArgsConstructor
 public class CertificationLikeController {
     private final CertificationLikeService certificationLikeService;
 
-    @Operation(summary = "미션 인증 좋아요")
+    @Operation(summary = "미션 인증 좋아요 토글")
     @PostMapping("/{certificationId}")
-    public CertificationLikeResponse createCertificationLike(@PathVariable Long certificationId) {
-        return certificationLikeService.createCertificationLike(certificationId);
+    public CertificationLikeResponse createCertificationLike(@PathVariable Long certificationId, @RequestBody CreateCertificationLikeRequest createCertificationLikeRequest) {
+        return certificationLikeService.createCertificationLike(certificationId, createCertificationLikeRequest);
     }
 
-    @Operation(summary = "미션 인증 좋아요 취소")
-    @DeleteMapping("/{certificationId}")
-    public void deleteCertificationLike(@PathVariable Long certificationId) { certificationLikeService.deleteCertificationLike(certificationId); }
 }
